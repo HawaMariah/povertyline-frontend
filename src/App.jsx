f1386630be31c5530ad35615803a9131ddbbb3aa
mercy-chepchirchir <mercychepchirchir33@gmail.com>
2023-10-29 20:05:43 +0300

    set up tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,11 @@ function App() {
   
 
   return (
-    <>
-    <div>
-     <Landing/>
-    </div>
+    <h1 className="text-3xl font-bold underline text-red-600">
+    Hello world!
      
-    </>
+    </h1>
+  
   )
 }
 
