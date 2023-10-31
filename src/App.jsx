88461dd2b3057bd5e22acde7649086df813d2a09
Hawa <hawamaria33@gmail.com>
2023-10-31 12:43:20 +0300

    postjob form

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import Landing from './components/Landing'
 import './App.css'
-
+import Postjob from './components/Postjob'
 function App() {
   
 
   return (
     <>
     <div>
-     <Landing/>
+     {/* <Landing/> */}
+     <Postjob/>
     </div>
      
     </>
