8d6c4647bd55a6f9da191ee5dd3d5633d0d60ce2
Hawa <hawamaria33@gmail.com>
2023-11-02 13:13:17 +0300

    updates

--- src/components/Lfooter.jsx
+++ src/components/Lfooter.jsx
@@ -9,36 +9,36 @@ function Lfooter() {
   return (
     <>
       <div
-        className="text-2xl  pt-7 pb-4 bg-[#bad9d8] flex justify-around"
+        className="text-2xl  pt-9 bg-[#bad9d8] flex justify-around"
         style={{
           fontFamily: "junge",
         }}
       >
         <div>
           <p className="text-gray-900 pb-2">Microsoft</p>
-          <img src={micro} alt="" className="w-[4.11vw] h-[6.vh] pl-2 " />
+          <img src={micro} alt="" className="w-[5.11vw] h-[6.vh] pl-2 " />
         </div>
 
         <div>
           <p className="text-gray-900  pb-2">Google</p>
-          <img src={google} alt="" className="w-[4.11vw] h-[6.vh] " />
+          <img src={google} alt="" className="w-[5.11vw] h-[6.vh] " />
         </div>
 
         <div>
           <p className="text-gray-900  pb-2">Dropbox</p>
-          <img src={drop} alt="" className="w-[4.11vw] h-[6.vh] " />
+          <img src={drop} alt="" className="w-[5.11vw] h-[6.vh] " />
         </div>
 
         <div>
           <p className="text-gray-900  pb-2">Airbnb</p>
           <div className="">
-          <img src={bnb} alt="" className="w-[4.11vw] h-[6.vh]   " />
+          <img src={bnb} alt="" className="w-[5.11vw] h-[6.vh]   " />
           </div>
         </div>
 
         <div>
           <p className="text-gray-900  pb-2">Virgin</p>
-          <img src={virg} alt="" className="w-[4.11vw] h-[6.vh] " />
+          <img src={virg} alt="" className="w-[5.11vw] h-[6.vh] " />
         </div>
       </div>
     </>
