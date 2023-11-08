a1fdc259f141590b8a4a58a16aa80d4c6572ee84
Hawa Maria <129379726+HawaMariah@users.noreply.github.com>
2023-11-08 11:50:31 +0300

    Update Contactus.jsx

--- src/Components/Contactus.jsx
+++ src/Components/Contactus.jsx
@@ -1,10 +1,38 @@
+
 import React from "react";
 import img from "../assets/talk.jpg";
+import { useRef } from "react";
+import emailjs from "@emailjs/browser";
 
 export default function ContactUs() {
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_55um8t8",
+        "template_zuepu9o",
+        form.current,
+        "8tP4AOP9GMJigUvPp"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          form.current.reset();
+          alert ('message succesfully sent')
+        
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+   
+  };
+
   return (
     <>
-    <div className="relative mb-5">
+      <div className="relative mb-5">
         <img
           src={img}
           alt=""
@@ -29,9 +57,10 @@ export default function ContactUs() {
                 <div className="mb-12 md:mt-12 lg:mt-0 lg:mb-0">
                   <div className="relative z-[1] block rounded-lg bg-[hsla(0,0%,100%,0.55)] px-6 py-12 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] backdrop-blur-[30px] dark:bg-[hsla(0,0%,5%,0.7)] dark:shadow-black/20 md:px-12 lg:-mr-14">
                     <h2 className="mb-12 text-3xl font-bold">Contact us</h2>
-                    <form>
+                    <form ref={form} onSubmit={sendEmail}>
                       <div className="relative mb-6" data-te-input-wrapper-init>
                         <input
+                          name="user_name"
                           type="text"
                           className="peer block min-h-[auto] w-full rounded border-0 bg-transparent py-[0.32rem] px-3 leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:peer-focus:text-primary [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
                           id="exampleInput90"
@@ -47,6 +76,7 @@ export default function ContactUs() {
                       <div className="relative mb-6" data-te-input-wrapper-init>
                         <input
                           type="email"
+                          name="user_email"
                           className="peer block min-h-[auto] w-full rounded border-0 bg-transparent py-[0.32rem] px-3 leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:peer-focus:text-primary [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
                           id="exampleInput91"
                           placeholder="Email address"
@@ -60,6 +90,7 @@ export default function ContactUs() {
                       </div>
                       <div className="relative mb-6" data-te-input-wrapper-init>
                         <textarea
+                          name="message"
                           className="peer block min-h-[auto] w-full rounded border-0 bg-transparent py-[0.32rem] px-3 leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
                           id="exampleFormControlTextarea1"
                           rows="3"
@@ -74,7 +105,8 @@ export default function ContactUs() {
                       </div>
 
                       <button
-                        type="button"
+                        type="submit"
+                        value="Send"
                         data-te-ripple-init
                         data-te-ripple-color="light"
                         className="inline-block w-full rounded bg-primary px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] lg:mb-0"
@@ -87,7 +119,7 @@ export default function ContactUs() {
                 <div className="md:mb-12 lg:mb-0">
                   <div className="relative h-[700px] rounded-lg shadow-lg dark:shadow-black/20">
                     <iframe
-                      src="https://maps.google.com/maps?q=manhattan&t=&z=13&ie=UTF8&iwloc=&output=embed"
+                      src={`https://maps.google.com/maps?q=${-1.286389},${36.817223}&t=&z=13&ie=UTF8&iwloc=&output=embed`}
                       className="absolute left-0 top-0 h-full w-full rounded-lg"
                       frameBorder="0"
                       allowFullScreen
@@ -99,7 +131,7 @@ export default function ContactUs() {
           </div>
         </section>
       </div>
-      
+     
     </>
   );
 }
