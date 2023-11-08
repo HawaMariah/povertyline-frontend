8259c275a077f8fe3867edb9c19fcfcd34e42b94
Hawa <hawamaria33@gmail.com>
2023-11-08 11:56:40 +0300

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Landing from "./Components/Landing";
 import "./index.css";
 // import Postjob from "./components/Postjob";
-// import Contactus from "./components/Contactus";
+//import ContactUs from "./Components/Contactus";
 import SignUp from "./Components/Signup";
 import Community from "./Components/Community";
 import Home from "./Components/Home";
@@ -15,17 +15,17 @@ function App() {
       <div>
        
        
-          <Routes>
+           <Routes>
           <Route exact path="/" element={<Landing />} />
             <Route exact path="/signup" element={<SignUp />} />
             <Route exact path="/community" element={<Community />} />
             <Route exact path="/findjobs" element={<Home />} />
           
-          </Routes>
-         
+          </Routes> 
+          
         
         {/* <Postjob/> */}
-        {/* <Contactus/> */}
+       
        
 
        
