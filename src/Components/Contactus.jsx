1f96938a6e99fa13cdff25d6e1a429b9850e2455
Hawa <hawamaria33@gmail.com>
2023-11-09 21:26:23 +0300

    contact update

--- src/Components/Contactus.jsx
+++ src/Components/Contactus.jsx
@@ -5,7 +5,7 @@ import emailjs from "@emailjs/browser";
 
 export default function ContactUs() {
   useEffect(() => {
-    emailjs.init("YOUR-PUBLIC-KEY-HERE")
+    emailjs.init("8tP4AOP9GMJigUvPp")
   }, [])
 
   const form = useRef();
@@ -13,15 +13,15 @@ export default function ContactUs() {
   const sendEmail = (e) => {
     e.preventDefault();
 
-    const serviceId = "service_o77mhct"
-    const templateId = "template_u70t73l"
+    const serviceId = "service_55um8t8"
+    const templateId = "template_zuepu9o"
 
     emailjs
       .sendForm(
         serviceId,
         templateId,
         form.current,
-        "rZNjwCu7QYQwYwSoa"
+        "8tP4AOP9GMJigUvPp"
       )
       .then(
         (result) => {
@@ -61,8 +61,8 @@ export default function ContactUs() {
           <div className="py-12 md:px-6">
             <div className="container mx-auto xl:px-32">
               <div className="grid items-center lg:grid-cols-2">
-                <div className="mb-12 md:mt-12 lg:mt-0 lg:mb-0 mr-10">
-                  <div className="relative z-[1] block rounded-lg bg-[hsla(0,0%,100%,0.55)] px-6 py-12 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] backdrop-blur-[30px] dark:bg-[hsla(0,0%,5%,0.7)] dark:shadow-black/20 md:px-12 lg:-mr-14">
+                <div className="relative    mb-12 md:mt-12 lg:mt-0 lg:mb-0 mr-10">
+                  <div className=" relative  z-[1] block rounded-lg  px-6 py-12 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] backdrop-blur-[30px] dark:bg-[hsla(0,0%,5%,0.7)] dark:shadow-black/20 md:px-12 lg:-mr-14">
                     <h2 className="mb-12 text-3xl font-bold">Contact us</h2>
                     <form className="space-y-7" ref={form} onSubmit={sendEmail}>
                       <div className="relative mb-6" data-te-input-wrapper-init>
@@ -92,7 +92,7 @@ export default function ContactUs() {
                         <input
                           type="email"
                           name="user_email"
-                          className="peer block min-h-[auto] w-full rounded border-0 py-[0.32rem] px-3 leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:peer-focus:text-primary [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+                          className="peer block min-h-[auto] w-full rounded border-0 py-[0.32rem] px-3 leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none  dark:placeholder:text-neutral-200 dark:peer-focus:text-primary [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
                           id="exampleInput91"
                           placeholder="Email address"
                         />
@@ -107,7 +107,7 @@ export default function ContactUs() {
 
                         <textarea
                           name="message"
-                          className="peer block min-h-[auto] w-full rounded border-0 py-[0.32rem] px-3 leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+                          className="peer block min-h-[auto] w-full rounded border-0 py-[0.32rem] px-3 leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none  dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
                           id="exampleFormControlTextarea1"
                           rows="3"
                           placeholder="Your message"
